823805b4557945c036d09dbff270a1d5644275b0
Nguyễn Duy Đức Chính <96870995+BlackZ36@users.noreply.github.com>
2024-04-11 20:06:16 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,16 +176,6 @@
     }
   });
 
-  /**
-   * Init swiper sliders
-   */
-  function initSwiper() {
-    document.querySelectorAll('.swiper').forEach(function(swiper) {
-      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
-      new Swiper(swiper, config);
-    });
-  }
-  window.addEventListener('load', initSwiper);
 
   /**
    * Animation on scroll function and init
